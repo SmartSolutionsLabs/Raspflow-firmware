0d1cc846de0cdd2c23986b256dec7c8fd6706430
el-crag <azertyggmf@outlook.com>
2024-08-28 11:53:18 -0500

    Added value attribute in Sensor

--- include/Sensor.hpp
+++ include/Sensor.hpp
@@ -4,12 +4,19 @@
 #include <Module.hpp>
 
 class Sensor : public Module {
+	private:
+		unsigned int value = 0;
+
 	public:
 		Sensor(const char * name, int taskCore = 1);
 
 		void connect(void * data) override;
 
 		void run(void* data) override;
+
+		unsigned int getValue() const {
+			return value;
+		}
 };
 
 #endif
